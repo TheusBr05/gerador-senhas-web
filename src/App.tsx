7a79cb1431a1ddab3eecabfdd1639e19d39d2c19
TheusBr05 <mat.587263@gmail.com>
2025-03-28 16:30:01 -0300

    Adiciona lógica de roteamento e componente Sidebar ao App

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Sidebar } from './components/sidebar/Sidebar'
+import { FiHome } from 'react-icons/fi'
+import { JSX } from 'react/jsx-runtime'
+import { SidebarSubitemProps } from './components/sidebar-subitem/SidebarSubitem'
+import { MdOutlineVpnKey } from 'react-icons/md'
+
+interface SidebarItem {
+  name: string;
+  icon: JSX.Element;
+  path: string;
+  subitems?: SidebarSubitemProps[] | undefined
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const itemsSidebar: SidebarItem[] = [
+    {
+      name: 'Home',
+      icon: <FiHome />,
+      path: '/',
+    },
+    {
+      name: 'Senhas Salvas',
+      icon: <MdOutlineVpnKey />,
+      path: '/senhas-salvas',
+    }
+  ]
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Sidebar items={itemsSidebar} />}>
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
